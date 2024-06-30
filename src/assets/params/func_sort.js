f47719af37a62391a7b58743651b7c741c1a76a2
BanceSt <59577061+BanceSt@users.noreply.github.com>
2024-06-30 12:57:14 -0300

    add func sorting

--- src/assets/params/func_sort.js
+++ src/assets/params/func_sort.js
@@ -12,14 +12,56 @@ export const sorts_types  = [
     },
     {
         "name" : "alphabéthique author-title",
-        "func" : () => {
+        "func" : (array) => {
 
+            //function de sort author-title
+            function sortDeviant (a, b) {
+                // enlever les espaces et mettre en maj
+                let author_a = a.author.trim().toUpperCase();
+                let author_b = b.author.trim().toUpperCase();
+            
+                if (author_a === author_b) {
+                    // enlever les espaces et mettre en maj
+                    let title_a = a.title.trim().toUpperCase();
+                    let title_b = b.title.trim().toUpperCase();
+            
+                    if (title_a < title_b) return -1;
+                    else if (title_a > title_b) return 1;
+                    else return 0;
+                } else if (author_a < author_b) return -1;
+                else return 1;
+            }
+
+            // utilisation de la fonction
+            array.sort(sortDeviant);
         }
     },
     {
         "name" : "alphabéthique title-author",
-        "func" : () => {
+        "func" : (array) => {
 
+            //function de sort title-author
+            function sortDeviant (a, b) {
+                // enlever les espaces et mettre en maj
+                let title_a = a.title.trim().toUpperCase();
+                let title_b = b.title.trim().toUpperCase();
+                
+            
+                if (title_a === title_b) {
+                    // enlever les espaces et mettre en maj
+                    let author_a = a.author.trim().toUpperCase();
+                    let author_b = b.author.trim().toUpperCase();
+                    
+            
+                    if (author_a < author_b) return -1;
+                    else if (author_a > author_b) return 1;
+                    else return 0;
+                } else if (title_a < title_b) return -1;
+                else return 1;
+            }
+
+             // utilisation de la fonction
+             array.sort(sortDeviant);
         }
     },
 ]
