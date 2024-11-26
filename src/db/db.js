858c85c35517d1e4332f8793bc302bf6ed96f751
BanceSt <59577061+BanceSt@users.noreply.github.com>
2024-11-26 10:16:18 -0400

    fix db

--- src/db/db.js
+++ src/db/db.js
@@ -6,10 +6,10 @@ export const db = new Dexie("MyDatabase");
 // Définition des tables
 db.version(1).stores({
     sorts: "++id, sort_type", // Table sorts avec clé primaire auto-incrémentée
-    deviations_sorted: "++id, sort_id, deviation_title, deviation_author" // sort_id est la clé étrangère simulée
+    deviations_sorted: "++id, sort_id, deviation_id, deviation_title, deviation_author" // sort_id est la clé étrangère simulée
 });
 
-// Exemple de méthode pour ajouter un sort et ses déviations associées
+// Méthode pour ajouter un sort et ses déviations associées
 export const addSortWithDeviations = async (sortType, deviations) => {
     return await db.transaction("rw", db.sorts, db.deviations_sorted, async () => {
         // Ajouter le sort
@@ -19,6 +19,7 @@ export const addSortWithDeviations = async (sortType, deviations) => {
         for (let deviation of deviations) {
             await db.deviations_sorted.add({
                 sort_id: sortId, // Référence au sort
+                deviation_id: deviation.id,
                 deviation_title: deviation.title,
                 deviation_author: deviation.author
             });
